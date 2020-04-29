76ff7d80990bdc5412ab8b74724d2667a1885918
tanyel.tuncer@spiegelburg.biz <tanyel.tuncer@spiegelburg.biz>
2020-04-29 09:08:51 +0200

    Changed version number

--- src/Simplic.PugIn.Ftp/Init.cs
+++ src/Simplic.PugIn.Ftp/Init.cs
@@ -43,7 +43,7 @@ namespace Simplic.PlugIn.Ftp
     /// <summary>
     /// Root PlugIn class
     /// </summary>
-    [PlugInDesc("Simplic Ftp", "1.0.119.1202", "D02206F4-26CD-4026-8B4A-81BFF978A0C2")]
+    [PlugInDesc("Simplic Ftp", "1.0.20.429", "D02206F4-26CD-4026-8B4A-81BFF978A0C2")]
     public class Init
     {
         /// <summary>
