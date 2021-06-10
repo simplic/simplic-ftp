5613c38ae6be94257f8e34d8f2b147fd7c71d2b8
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-06-10 11:32:34 +0200

    Add comments to Simplic.Ftp.Flow

--- src/Simplic.Ftp.Flow/DownloadFileNode.cs
+++ src/Simplic.Ftp.Flow/DownloadFileNode.cs
@@ -1,11 +1,12 @@
 ﻿using Simplic.Flow;
-using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace Simplic.Ftp.Flow
 {
+    /// <summary>
+    /// Node to download a file from an ftp server.
+    /// </summary>
     [ActionNodeDefinition(Name = nameof(DownloadFileNode), DisplayName = "Download file form ftp directory", Category = "FTP")]
     public class DownloadFileNode : ActionNode
     {
@@ -13,11 +14,20 @@ namespace Simplic.Ftp.Flow
         private IFtpService ftpService;
         private Dictionary<string, IFtpService> serviceCache;
 
+        /// <summary>
+        /// Initializes a new instance of DownloadFileNode.
+        /// </summary>
         public DownloadFileNode()
         {
             serviceCache = new Dictionary<string, IFtpService>();
         }
 
+        /// <summary>
+        /// Executes the node.
+        /// </summary>
+        /// <param name="runtime"></param>
+        /// <param name="scope"></param>
+        /// <returns></returns>
         public override bool Execute(IFlowRuntimeService runtime, DataPinScope scope)
         {
             if (ftpServerService == null)
@@ -34,22 +44,32 @@ namespace Simplic.Ftp.Flow
             }
             var filename = scope.GetValue<string>(InPinFileName);
             var path = scope.GetValue<string>(InPinPath);
-            var file = ftpService.DownloadFile(server, path + filename );
+            var file = ftpService.DownloadFile(server, path + filename);
             scope.SetValue(OutPinFile, file);
             runtime.EnqueueNode(OutNodeSuccess, scope);
 
             return true;
         }
 
-
-
+        /// <summary>
+        /// Gets the name of the node.
+        /// </summary>
         public override string Name => nameof(DownloadFileNode);
 
+        /// <summary>
+        /// Gets the friendly name of the node.
+        /// </summary>
         public override string FriendlyName => nameof(DownloadFileNode);
 
+        /// <summary>
+        /// Gets or sets the success out node.
+        /// </summary>
         [FlowPinDefinition(DisplayName = "Success", Name = "OutNodeSuccess", PinDirection = PinDirection.Out)]
         public ActionNode OutNodeSuccess { get; set; }
 
+        /// <summary>
+        /// Gets or sets the in pin for the server name.
+        /// </summary>
         [DataPinDefinition(
             Id = "B70916E1-7E06-4D75-9334-5652624313DC",
             ContainerType = DataPinContainerType.Single,
@@ -59,6 +79,9 @@ namespace Simplic.Ftp.Flow
             DataType = typeof(string))]
         public DataPin InPinServer { get; set; }
 
+        /// <summary>
+        /// Gets or sets the in pin for the path.
+        /// </summary>
         [DataPinDefinition(
             Id = "55A3798F-100B-4F1D-AF7D-CB175C4AE9AB",
             ContainerType = DataPinContainerType.Single,
@@ -66,8 +89,11 @@ namespace Simplic.Ftp.Flow
             Name = "InPinPath",
             DisplayName = "Path",
             DataType = typeof(string))]
-        public DataPin InPinPath{ get; set; }
+        public DataPin InPinPath { get; set; }
 
+        /// <summary>
+        /// Gets or sets the in pin for the file name.
+        /// </summary>
         [DataPinDefinition(
             Id = "E611D837-F95C-4BFF-BFCE-743654BC2640",
             ContainerType = DataPinContainerType.Single,
@@ -77,6 +103,9 @@ namespace Simplic.Ftp.Flow
             DataType = typeof(string))]
         public DataPin InPinFileName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the out pin for the file.
+        /// </summary>
         [DataPinDefinition(
             Id = "14248A85-9D95-41E3-A460-0C6E1B5D397B",
             ContainerType = DataPinContainerType.Single,
--- src/Simplic.Ftp.Flow/UploadFileToFtpNode.cs
+++ src/Simplic.Ftp.Flow/UploadFileToFtpNode.cs
@@ -5,6 +5,9 @@ using System.Linq;
 
 namespace Simplic.Ftp.Flow
 {
+    /// <summary>
+    /// Node to upload a file to a ftp server.
+    /// </summary>
     [ActionNodeDefinition(Name = nameof(UploadFileToFtpNode), DisplayName = "Upload file to ftp server", Category = "FTP")]
     public class UploadFileToFtpNode : ActionNode
     {
@@ -12,11 +15,20 @@ namespace Simplic.Ftp.Flow
         private IFtpService ftpService;
         private Dictionary<string, IFtpService> serviceCache;
 
+        /// <summary>
+        /// Initializes a new instance of UploadFileToFtpNode.
+        /// </summary>
         public UploadFileToFtpNode()
         {
             serviceCache = new Dictionary<string, IFtpService>();
         }
 
+        /// <summary>
+        /// Executes the node.
+        /// </summary>
+        /// <param name="runtime"></param>
+        /// <param name="scope"></param>
+        /// <returns></returns>
         public override bool Execute(IFlowRuntimeService runtime, DataPinScope scope)
         {
             if (ftpServerService == null)
@@ -39,7 +51,7 @@ namespace Simplic.Ftp.Flow
                 ftpService.UploadFile(server, file, path, fileName);
                 runtime.EnqueueNode(OutNodeSuccess, scope);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 runtime.EnqueueNode(OutNodeFaield, scope);
                 Console.WriteLine(ex);
@@ -47,16 +59,31 @@ namespace Simplic.Ftp.Flow
             return true;
         }
 
+        /// <summary>
+        /// Gets the name of the node.
+        /// </summary>
         public override string Name => nameof(UploadFileToFtpNode);
 
+        /// <summary>
+        /// Gets the friendly name of the node.
+        /// </summary>
         public override string FriendlyName => nameof(UploadFileToFtpNode);
 
+        /// <summary>
+        /// Gets or sets the success out node.
+        /// </summary>
         [FlowPinDefinition(DisplayName = "Success", Name = nameof(OutNodeSuccess), PinDirection = PinDirection.Out)]
         public ActionNode OutNodeSuccess { get; set; }
 
+        /// <summary>
+        /// Gets or sets the failed out node.
+        /// </summary>
         [FlowPinDefinition(DisplayName = "Failed", Name = nameof(OutNodeFaield), PinDirection = PinDirection.Out)]
         public ActionNode OutNodeFaield { get; set; }
 
+        /// <summary>
+        /// Gets oe sets the in pin for the server name.
+        /// </summary>
         [DataPinDefinition(
             Id = "972CE8E3-7FC8-47B4-B3B1-37071A3B35A8",
             ContainerType = DataPinContainerType.Single,
@@ -66,7 +93,9 @@ namespace Simplic.Ftp.Flow
             DataType = typeof(string))]
         public DataPin InPinServer { get; set; }
 
-
+        /// <summary>
+        /// Gets or sets the in pin for the file.
+        /// </summary>
         [DataPinDefinition(
             Id = "A0B7A136-BDB4-4FCB-9BC2-E7E30AC599CF",
             ContainerType = DataPinContainerType.Single,
@@ -76,6 +105,9 @@ namespace Simplic.Ftp.Flow
             DataType = typeof(byte[]))]
         public DataPin InPinFile { get; set; }
 
+        /// <summary>
+        /// Gets or sets the in pin for the file name.
+        /// </summary>
         [DataPinDefinition(
             Id = "B0F691AE-E483-411B-ADD2-18E7938F3176",
             ContainerType = DataPinContainerType.Single,
@@ -85,6 +117,9 @@ namespace Simplic.Ftp.Flow
             DataType = typeof(string))]
         public DataPin InPinFileName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the in pin for the path.
+        /// </summary>
         [DataPinDefinition(
             Id = "0A750D38-446A-4E0C-970A-915D93DF6AD8",
             ContainerType = DataPinContainerType.Single,
