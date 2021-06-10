4bf4b04ec85ae1afaa3057ff06f7b5b57a47bacb
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2021-06-10 12:07:47 +0200

    Add comments to Simplic.Ftp.UI

--- src/Simplic.Ftp.UI/IFtpServerWindow.cs
+++ src/Simplic.Ftp.UI/IFtpServerWindow.cs
@@ -3,6 +3,9 @@ using System;
 
 namespace Simplic.Ftp.UI
 {
+    /// <summary>
+    /// Window interface for ftp server windows.
+    /// </summary>
     public interface  IFtpServerWindow : IWindow<Guid, FtpServerConfiguration, FtpServerViewModel>
     {
     }
